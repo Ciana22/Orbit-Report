e58ad4a11125b52743b17cdd92d98b90297df6de
Laciana Hughes <laciana_hughes@cable.comcast.com>
2021-06-21 12:31:25 -0400

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,10 +26,14 @@ export class AppComponent {
         for (let i = 0; i < fetchedSatellites.length; i++) {
           let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
           this.sourceList.push(satellite);
+          this.displayList = this.sourceList.slice(0);
+    
         }
       }.bind(this));
     }.bind(this));
   }
+
+  
   search(searchTerm: string): void {
     let matchingSatellites: Satellite[] = [];
     searchTerm = searchTerm.toLowerCase();
@@ -42,6 +46,7 @@ export class AppComponent {
     // assign this.displayList to be the array of matching satellites
     // this will cause Angular to re-make the table, but now only containing matches
     this.displayList = matchingSatellites;
+    
   }
 }
 
